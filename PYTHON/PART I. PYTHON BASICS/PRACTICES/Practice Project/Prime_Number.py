e214ff819745d75e0e4213b2401424d0159395e7
Dark Hunter <122430059+DarkHunter6@users.noreply.github.com>
2023-01-11 17:40:45 +0530

    Delete PYTHON/PART I. PYTHON BASICS/PRACTICES directory

--- PYTHON/PART I. PYTHON BASICS/PRACTICES/Practice Project/Prime_Number.py
+++
@@ -1,7 +0,0 @@
-def PrimeNumber():
-    Number = input('Enter Any Number : ')
-    if int(Number) % 2 == 0:
-        print('The Number Is Not Prime')
-    else:
-        print('The Number Is Prime')
-PrimeNumber()
