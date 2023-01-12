524763c90731e8c689e0bd46f6dfaf44c58f73f0
Python Paul <122430059+DarkHunter6@users.noreply.github.com>
2023-01-12 10:10:06 +0530

    Delete PYTHON/PART II. AUTOMATING TASKS/CHAPTER 7. PATTERN MATCHING WITH REGULAR EXPRESSION directory

--- PYTHON/PART II. AUTOMATING TASKS/CHAPTER 7. PATTERN MATCHING WITH REGULAR EXPRESSION/Finding Patterns Of Text With Regular Expression.py
+++
@@ -1,5 +0,0 @@
-import re
-# PhoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # Regex - Regular Expression
-PhoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}') # re.compile -> Here 'Compile' Is A Object Of re
-Mobile = PhoneNumRegex.search('Call Me At 863-781-3299 Tommorrow.')
-print('Phone Number Found : ' + Mobile.group()) # .group Helps Us To Show The Matches
--- PYTHON/PART II. AUTOMATING TASKS/CHAPTER 7. PATTERN MATCHING WITH REGULAR EXPRESSION/Matching Multiple Groups With The Pipe.py
+++
@@ -1,5 +0,0 @@
-import re
-
-Name = re.compile(r'Tushar Paul | DarkEagle')
-Group_1 = Name.search('Tushar Paul And DarkEagle') # Get The First String Search
-print(Group_1.group())
