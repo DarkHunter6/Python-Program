4858ee749af78e7a5651d7d33a47d67ee43f6142
PyGuy <122430059+DarkHunter6@users.noreply.github.com>
2023-01-13 11:48:10 +0530

    Update MADLIBS.py

--- PYTHON/OTHER PROGRAMS/Matlibs/MADLIBS.py
+++ PYTHON/OTHER PROGRAMS/Matlibs/MADLIBS.py
@@ -5,4 +5,4 @@ Destination = input('Where Are You Going : ')
 Long = input('For How Long : ')
 
 print(f'''You Have A Nice And Powerfull Muscle Car {Car_Name}...It Had So May Popularity Back In {Released_In}...So, This Time You have {Carry} Friends...Ahh...
-{Destination} Is A Nice Place For Vacation And I Think It's Gonna Be A {Long} Vacation Guys...Good Luck...!''')
+{Destination} Is A Nice Place For Vacation And I Think It's Gonna Be {Long} Vacation Guys...Good Luck...!''')
