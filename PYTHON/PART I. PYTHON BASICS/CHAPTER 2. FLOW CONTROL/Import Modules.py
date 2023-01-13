6c686e3d7cce3e29a69cd6a78802b36ee37400f9
PyGuy <122430059+DarkHunter6@users.noreply.github.com>
2023-01-13 12:03:59 +0530

    Update Import Modules.py

--- PYTHON/PART I. PYTHON BASICS/CHAPTER 2. FLOW CONTROL/Import Modules.py
+++ PYTHON/PART I. PYTHON BASICS/CHAPTER 2. FLOW CONTROL/Import Modules.py
@@ -1,7 +1,7 @@
-# from random import *
-# for i in range(5):
-#     print(randint(1, 10))
+from random import *
+ for i in range(5):
+     print(randint(1, 10))
 
-# import random 
-# for i in range(5):
-#     print(random.randint(1, 10)) # Between Two Numbers
+import random 
+ for i in range(5):
+     print(random.randint(1, 10)) # Between Two Numbers
