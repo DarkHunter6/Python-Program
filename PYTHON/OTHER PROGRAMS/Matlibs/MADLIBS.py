0199f237ac0bc17a2f14bfcb4fa73d5c0a0f1792
PyGuy <122430059+DarkHunter6@users.noreply.github.com>
2023-01-13 11:59:42 +0530

    Update MADLIBS.py

--- PYTHON/OTHER PROGRAMS/Matlibs/MADLIBS.py
+++ PYTHON/OTHER PROGRAMS/Matlibs/MADLIBS.py
@@ -6,3 +6,5 @@ Long = input('For How Long : ')
 
 print(f'''You Have A Nice And Powerfull Muscle Car {Car_Name}...It Had So May Popularity Back In {Released_In}...So, This Time You have {Carry} Friends...Ahh...
 {Destination} Is A Nice Place For Vacation And I Think It's Gonna Be {Long} Vacation Guys...Good Luck...!''')
+
+# We Used 'f-string' Or 'Literal String Interpolation'.
