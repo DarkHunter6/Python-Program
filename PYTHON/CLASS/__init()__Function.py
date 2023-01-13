4945b288e69f9f42cd08c49958e2168f86665c79
PyGuy <122430059+DarkHunter6@users.noreply.github.com>
2023-01-13 11:54:29 +0530

    Update __init()__Function.py

--- PYTHON/CLASS/__init()__Function.py
+++ PYTHON/CLASS/__init()__Function.py
@@ -1,6 +1,6 @@
 class Person:
   def __init__(self, name, age): 
-# All classes have a function called __init__(), which is always executed when the class is being initiated(Class Start Hoi Jokhon).
+# All classes have a function called __init__(), which is always executed when the class is being initiated(Wher Class Is Being Started).
 # Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
     self.name = name # self(Parameter).name(Variable Of self) = name(Parameter)
     self.age = age # Self(Parameter).age(Variable Of self) = age(Parameter)
