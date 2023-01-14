d49306dc35693e27fcac6f2ae485057a1471a4fa
PyGuy <122430059+DarkHunter6@users.noreply.github.com>
2023-01-14 10:23:46 +0530

    Update Get_Method.py

--- PYTHON/PART I. PYTHON BASICS/CHAPTER 5. DICTIONARIES AND STRUCTURING DATA/Get_Method.py
+++ PYTHON/PART I. PYTHON BASICS/CHAPTER 5. DICTIONARIES AND STRUCTURING DATA/Get_Method.py
@@ -1,2 +1,2 @@
 Cars = {'Car' : 'Bugatti'}
-print('I Have ' + str(Cars.get('Car')) + ' Do You Want To Race With Me Now MotherFucker') # The Fallback Value Is 'None'
+print('I Have ' + str(Cars.get('Car')) + ' Do You Want To Race With Me Now') # The Fallback Value Is 'None'
